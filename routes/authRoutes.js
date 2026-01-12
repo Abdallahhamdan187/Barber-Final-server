f4c420db5459bc81e6cb75faa909b1de0570ce41
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-12 17:16:58 +0300

    feat: authentication API routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,33 @@
+import express from "express";
+import pgclient from "../db.js";
+const authRoutes = express.Router();
+// Define authentication-related routes here
+
+// localhost:5000/api/auth/login
+authRoutes.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    const result = await pgclient.query("SELECT user_id, full_name, email, role FROM barberschema.users WHERE email = $1 AND password = $2", [email, password]);
+    if (result.rows.length === 0) {
+        return res.status(401).json({ error: "Invalid email or password" });
+    }
+    res.json(result.rows[0]);
+
+
+});
+// localhost:5000/api/auth/signup
+authRoutes.post("/signup", async (req, res) => {
+    const { full_name, email, password } = req.body;
+
+    const existingUser = await pgclient.query("SELECT * FROM barberschema.users WHERE email = $1", [email]);
+    if (existingUser.rows.length > 0) {
+        return res.status(400).json({ error: "Email already used" });
+    }
+    const result = await pgclient.query("INSERT INTO barberschema.users (full_name, email, password, role) VALUES ($1, $2, $3, 'user') RETURNING user_id, full_name, email, role",
+        [full_name, email, password]);
+    res.status(201).json(result.rows[0]);
+
+});
+
+
+export default authRoutes;
