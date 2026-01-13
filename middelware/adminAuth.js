202ba24b4f8b6e846caf80c1460fcb845ba42bfd
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-13 16:39:48 +0300

    enforce admin authorization middleware

--- middelware/adminAuth.js
+++ middelware/adminAuth.js
@@ -3,5 +3,5 @@ export default function adminAuth(req, res, next) {
 
     if (role === "admin") return next();
 
-    return res.status(403).json({ message: "Admin access only" });
+    return res.status(403).json({ message: "Admin access only!!" });
 }
