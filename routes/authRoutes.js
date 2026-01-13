ea2779995695f479ac1a1490200fd57b70d4f4df
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-13 16:29:32 +0300

    feat: improve auth routes error handeling

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,27 +6,31 @@ const authRoutes = express.Router();
 // localhost:5000/api/auth/login
 authRoutes.post("/login", async (req, res) => {
     const { email, password } = req.body;
-
-    const result = await pgclient.query("SELECT user_id, full_name, email, role FROM barberschema.users WHERE email = $1 AND password = $2", [email, password]);
-    if (result.rows.length === 0) {
-        return res.status(401).json({ error: "Invalid email or password" });
+    try {
+        const result = await pgclient.query("SELECT user_id, full_name, email, role FROM barberschema.users WHERE email = $1 AND password = $2", [email, password]);
+        if (result.rows.length === 0) {
+            return res.status(401).json({ error: "Invalid email or password" });
+        }
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
     }
-    res.json(result.rows[0]);
-
 
 });
 // localhost:5000/api/auth/signup
 authRoutes.post("/signup", async (req, res) => {
     const { full_name, email, password } = req.body;
-
-    const existingUser = await pgclient.query("SELECT * FROM barberschema.users WHERE email = $1", [email]);
-    if (existingUser.rows.length > 0) {
-        return res.status(400).json({ error: "Email already used" });
+    try {
+        const existingUser = await pgclient.query("SELECT * FROM barberschema.users WHERE email = $1", [email]);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ error: "Email already used" });
+        }
+        const result = await pgclient.query("INSERT INTO barberschema.users (full_name, email, password, role) VALUES ($1, $2, $3, 'user') RETURNING user_id, full_name, email, role",
+            [full_name, email, password]);
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
     }
-    const result = await pgclient.query("INSERT INTO barberschema.users (full_name, email, password, role) VALUES ($1, $2, $3, 'user') RETURNING user_id, full_name, email, role",
-        [full_name, email, password]);
-    res.status(201).json(result.rows[0]);
-
 });
 
 
